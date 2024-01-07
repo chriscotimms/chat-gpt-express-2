a6b0730f29f37a8469e189cc5e30c7a8c0ae8c20
chriscotimms <chriscotimms@gmail.com>
2024-01-07 13:18:44 +0000

    working retrieval of messgaes, tutorial finished. Now to implement my own working version

--- openai.js
+++ openai.js
@@ -21,10 +21,42 @@ const openai = new OpenAI({
   // assistant_id = asst_OaCROTWTGyCg14jRXrc1o6gz
 
 
+// const assistant = await openai.beta.assistants.retrieve("asst_OaCROTWTGyCg14jRXrc1o6gz");
+// console.log(assistant);
 
-  const assistant = await openai.beta.assistants.retrieve("asst_OaCROTWTGyCg14jRXrc1o6gz");
+//Threads
 
-  console.log(assistant);
+//create thread
+// const thread = await openai.beta.threads.create();
+// console.log(thread);
+
+//create message 
+// const message = await openai.beta.threads.messages.create(thread.id, {
+//   role: "user",
+//   content: "what is 500*500?"
+// });
+
+//run assistant
+// const run = await openai.beta.threads.runs.create(thread.id, {
+//   assistant_id: assistant.id,
+//   instructions: "Address the user as Chris"
+// });
+
+//retrieve latest thread response
+// const run = await openai.beta.threads.runs.retrieve(
+//   "thread_5Uml4fmUnIylPc4sVejJp0O6", 
+//   "run_5rdOtQ5vB8K1BZDuP058Ujyk"
+// );
+// console.log(run);
+
+
+const messages = await openai.beta.threads.messages.list("thread_5Uml4fmUnIylPc4sVejJp0O6");
+// console.log(messages);
+
+messages.body.data.forEach((message) => {
+  console.log(message.content);
+})
+
 
 // const port = process.env.PORT || 3000;
 // app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
